4e22cf2bf7f81c31bcea65954614669046854e0d
Gab_km <the25thcromosome@gmail.com>
2014-01-13 02:34:32 +0900

    initial commit

---
+++ MsgPack.Test/MsgPack.Test.fs
@@ -0,0 +1,49 @@
+﻿namespace MsgPack.Test
+
+open NUnit.Framework
+open MsgPack
+
+module TestExtensions =
+
+    let assertEqual<'a> expected (actual: 'a) = Assert.That(actual, Is.EqualTo(expected))
+    let assertEquivalentTo<'a> expected (actual: 'a) = Assert.That(actual, Is.EquivalentTo(expected))
+
+open TestExtensions
+
+[<TestFixture>]
+type MsgPackTest() = 
+    [<Test>]
+    member self.GivenTrueValueWhenSerializeBoolThenReturn0xc2() =
+        true |> Serialization.serializeBool |> assertEqual Format.True
+
+    [<Test>]
+    member self.GivenFalseValueWhenSerializeBoolThenReturn0xc3() =
+        false |> Serialization.serializeBool |> assertEqual Format.False
+
+    [<Test>]
+    member self.Given127WhenSerializeIntThenReturn0xff() =
+        127 |> Serialization.serializeInt |> assertEquivalentTo [| 0x7Fuy |]
+
+    [<Test>]
+    member self.Given128WhenSerializeIntThenReturn0xccf0() =
+        128 |> Serialization.serializeInt |> assertEquivalentTo [| Format.UInt8; 0x80uy |]
+        
+    [<Test>]
+    member self.Given255WhenSerializeIntThenReturn0xcd0100() =
+        255 |> Serialization.serializeInt |> assertEquivalentTo [| Format.UInt8; 0xFFuy |]
+
+    [<Test>]
+    member self.Given256WhenSerializeIntThenReturn0xcd0100() =
+        256 |> Serialization.serializeInt |> assertEquivalentTo [| Format.UInt16; 0x01uy; 0x00uy |]
+
+    [<Test>]
+    member self.Given0xffffWhenSerializeIntThenReturn0xcd7fff() =
+        0xFFFF |> Serialization.serializeInt |> assertEquivalentTo [| Format.UInt16; 0xFFuy; 0xFFuy |]
+
+    [<Test>]
+    member self.Given0x010000WhenSerializeIntThenReturn0xce010000() =
+        0x010000 |> Serialization.serializeInt |> assertEquivalentTo [| Format.UInt32; 0x00uy; 0x01uy; 0x00uy; 0x00uy |]
+
+    [<Test>]
+    member self.Given0x12345678WhenSerializeIntThenReturn0xce12345678() =
+        0x12345678 |> Serialization.serializeInt |> assertEquivalentTo [| Format.UInt32; 0x12uy; 0x34uy; 0x56uy; 0x78uy |]
---
+++ MsgPack/MsgPack.fs
@@ -0,0 +1,21 @@
+﻿namespace MsgPack
+
+type Format =
+    static member True = 0xc2uy
+    static member False = 0xc3uy
+    static member UInt8 = 0xccuy
+    static member UInt16 = 0xcduy
+    static member UInt32 = 0xceuy
+
+module Serialization =
+    let serializeBool value =
+        if value then Format.True
+        else Format.False
+
+    let serializeInt value =
+        if value < (1 <<< 8) then
+            if value < (1 <<< 7) then [| byte value |]
+            else [| Format.UInt8 ; byte value |]
+        else
+            if value < (1 <<< 16) then [| Format.UInt16; byte (value >>> 8); byte ((value <<< 8) >>> 8) |]
+            else [| Format.UInt32; byte (value >>> 24); byte (((value >>> 16) <<< 24) >>> 24); byte (((value >>> 8) <<< 24) >>> 24); byte ((value <<< 24) >>> 24) |]
