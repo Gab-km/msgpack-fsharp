25196a767b37e7f66dce519176ebc905b9fa935e
omanuke <kusokuzeshiki@gmail.com>
2015-10-26 21:00:30 +0900

    delete backup
    delete backup

--- Backup/MsgPack.Test/Extensions.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace MsgPack.Test
-
-open NUnit.Framework
-
-module Extensions =
-
-    let assertEqualTo<'a> (expected: 'a) (actual: 'a) = Assert.That(actual, Is.EqualTo(expected))
-    let assertEquivalentTo<'a> expected (actual: 'a) = Assert.That(actual, Is.EquivalentTo(expected))
